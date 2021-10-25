9762adebb99121b99c04af5901fc4321e17fc6ce
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-10-25 20:57:16 -0300

    Distancia em cm e mm

---
+++ metros.py
@@ -0,0 +1,5 @@
+d =float(input('Digite uma distancia em metros:'))
+centimetro = d*100
+milimetros = d*1000
+
+print(' A distância em cm é {} cm \n A distância em mm é {} mm'.format(centimetro,milimetros))
