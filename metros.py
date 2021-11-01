400f15ae53a479a03bca706392be20b4c5319b77
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-11-01 20:48:10 -0300

    Conversor de medidas

--- metros.py
+++ metros.py
@@ -2,4 +2,4 @@ d =float(input('Digite uma distancia em metros:'))
 centimetro = d*100
 milimetros = d*1000
 
-print(' A distância em cm é {} cm \n A distância em mm é {} mm'.format(centimetro,milimetros))
+print(' A distância em cm é {:.0f}cm \n A distância em mm é {:.0f}mm'.format(centimetro,milimetros))
