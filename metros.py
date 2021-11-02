b98515cee6f2dc63d7304d37ba97ab23126f82a3
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-11-01 21:01:27 -0300

    Conversor de medida

--- metros.py
+++ metros.py
@@ -1,5 +1,9 @@
 d =float(input('Digite uma distancia em metros:'))
-centimetro = d*100
-milimetros = d*1000
+km = d/1000
+hm = d/100
+dam = d/10
+dm = d*10
+cm = d*100
+mm = d*1000
 
-print(' A distância em cm é {:.0f}cm \n A distância em mm é {:.0f}mm'.format(centimetro,milimetros))
+print(' A distância em km é {}km \n A distância em hm é {}hm \n A distância em dam é {}dam \n A distância em dm é {:.0f}dm \n A distância em cm é {:.0f}cm \n A distância em mm é {:.0f}mm'.format(km,hm,dam,dm,cm,mm))
